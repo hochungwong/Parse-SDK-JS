8ce92d6e03375e27249d016ca339a9fd760f5202
carson wong <chung940712@gmail.com>
2021-11-22 12:52:37 +1100

    Changed const to let

--- src/EventEmitter.js
+++ src/EventEmitter.js
@@ -10,7 +10,7 @@
  */
 
 if (process.env.PARSE_BUILD === 'react-native') {
-  const EventEmitter = require('react-native/Libraries/vendor/emitter/EventEmitter');
+  let EventEmitter = require('react-native/Libraries/vendor/emitter/EventEmitter');
   if (EventEmitter.default) {
     EventEmitter = EventEmitter.default;
   }
