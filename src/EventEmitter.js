1e9ebb48030032540ee0fb2a5b4881d3bb594eb2
carson wong <chung940712@gmail.com>
2021-11-22 11:52:08 +1100

    changed import path

--- src/EventEmitter.js
+++ src/EventEmitter.js
@@ -10,7 +10,7 @@
  */
 
 if (process.env.PARSE_BUILD === 'react-native') {
-  let EventEmitter = require('../../../react-native/Libraries/vendor/emitter/EventEmitter');
+  import EventEmitter from 'react-native/Libraries/vendor/emitter/EventEmitter';
   if (EventEmitter.default) {
     EventEmitter = EventEmitter.default;
   }
